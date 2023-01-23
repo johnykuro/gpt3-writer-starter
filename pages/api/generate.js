42f840baf0c656be5904967136d14e22c638ce68
Jonathan Kuro <jon@jonkuro.com>
2023-01-23 12:52:28 +0000

    react update

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `I am talking to H.P. Lovecraft, answer as if you are the famous author. Be expressive in your response & speak as if you are a human from the early 19th century.`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
